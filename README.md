3c7c86ec581afee07d414c88d5863a89dfffcbb5
Onis-dwa <36276512+Onis-dwa@users.noreply.github.com>
2021-11-17 22:57:26 +0300

    v1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # StdAsyncPool
+This simple math program with using std::async threadpool
+
+The math problem is:
+Find all natural numbers belonging to the range [1 000 000; 2 000 000] where each number is factored to two factors in all possible ways and the set of difference of factors will contain at least three elements not exceeding 100.
+For example: for the number 16 we get the factors 16*1, 8*2, 4*4 and the difference 15, 6, 0
---
+++ StdAsyncPool.cpp
@@ -0,0 +1,79 @@
+﻿
+#include <iostream>
+#include <future>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+typedef int uint;
+
+static constexpr uint S_begin = 1'000'000;
+static constexpr uint S_end   = 2'000'000;
+static constexpr uint S_step  = 62'500;
+static mutex m;
+
+unsigned long long someF(uint l, uint r) {
+	m.lock();
+	cout << "thread for range " << l << " " << r << endl;
+	m.unlock();
+
+	unsigned long long iters = 0;
+	vector<pair<uint, uint>> result = {};
+	vector<pair<uint, uint>> resultDiffs = {};
+	for (uint i = l; i <= r; ++i) {
+		vector<pair<uint, uint>> mults = {};
+		uint sqr = sqrt(i);
+		uint cnt = 0;
+		for (uint a = sqr; a < sqr + 101; ++a) {
+			for (uint b = sqr;; --b) {
+				iters++;
+				uint mod = abs(a - b);
+				if (mod >= 100) break;
+				if (a * b == i) {
+					cnt += mod;
+					mults.push_back({ a, b });
+				}
+			}
+		}
+
+		if (mults.size() >= 3) {
+			result.push_back({ i, cnt });
+			for (const auto& it : mults)
+				resultDiffs.push_back(it);
+		}
+	}
+
+	m.lock();
+	for (const auto& it : result) {
+		cout << "i: " << it.first << " cnt: " << it.second << " wich [ ";
+		for (const auto& it : resultDiffs) {
+			cout << "{" << it.first << " " << it.second << "}, ";
+		}
+		cout << "\b\b ]" << endl;
+	}
+	m.unlock();
+	return iters;
+}
+
+int main() {
+	steady_clock::time_point begin = steady_clock::now();
+
+	vector<future<unsigned long long>> threads;
+	for (uint i = S_begin; i < S_end; i += S_step) {
+		threads.emplace_back(async(launch::async, someF, i, i + S_step));
+	}
+
+	unsigned long long summary = 0;
+	for (auto& it : threads) {
+		summary = it.get();
+	}
+	cout << "summary iterations " << summary << endl;
+
+	m.lock();
+	steady_clock::time_point end = steady_clock::now();
+	cout << "Total time: " << duration_cast<nanoseconds> (end - begin).count() << "[ns]" << endl;
+	m.unlock();
+
+	return 0;
+}
